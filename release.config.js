1491d8aa57a26ea205e0d1b466f7153d06224a68
Cody Olsen <stipsan@gmail.com>
2022-09-27 01:40:26 +0000

    fix: use new semantic release preset

--- release.config.js
+++ release.config.js
@@ -1,22 +1,4 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        tarballDir: 'release'
-      }
-    ],
-    [
-      '@semantic-release/github',
-      {
-        assets: 'release/*.tgz'
-      }
-    ],
-    '@semantic-release/git'
-  ],
-  preset: 'angular',
+  extends: '@sanity/semantic-release-preset',
   branches: ['main']
 }
